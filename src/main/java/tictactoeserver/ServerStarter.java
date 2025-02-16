529dfa959c8cca63d04d5991b1844264e0c1b691
yonhag <yonatanhagay06@gmail.com>
2025-02-16 23:11:40 +0200

    Almost Working

--- src/main/java/tictactoeserver/ServerStarter.java
+++ src/main/java/tictactoeserver/ServerStarter.java
@@ -3,7 +3,7 @@ package tictactoeserver;
 import java.io.IOException;
 
 public class ServerStarter {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ProgramManager pm = new ProgramManager();
     }
 }
